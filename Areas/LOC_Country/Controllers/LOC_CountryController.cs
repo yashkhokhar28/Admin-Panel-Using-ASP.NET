5909d7782095d609e779a624d4801c79c41d9c50
Yash Khokhar <yashkhokhar28@gmail.com>
2023-08-07 17:50:51 +0530

    Admin Panel

---
+++ Areas/LOC_Country/Controllers/LOC_CountryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdminPanel.Areas.LOC_Country.Controllers
+{
+    
+    [Area("LOC_Country")]
+    [Route("LOC_Country/[controller]/[action]")]
+    public class LOC_CountryController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private IConfiguration Configuration;
+
+        public LOC_CountryController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        public IActionResult LOC_CountryList()
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_Country_SelectAll";
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            return View(table);
+        }
+
+        public IActionResult LOC_CountryAddEdit()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Areas/LOC_Country/Models/LOC_CountryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminPanel.Areas.LOC_Country.Models
+{
+    public class LOC_CountryModel
+    {
+        public int CountryID { get; set; }
+        public string? CountryName { get; set; }
+        public string? CountryCode { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
