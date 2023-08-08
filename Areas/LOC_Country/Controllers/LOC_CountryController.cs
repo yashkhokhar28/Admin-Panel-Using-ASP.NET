d6a895bed65be06f89c04ae63cb6095b28936119
Yash Khokhar <yashkhokhar28@gmail.com>
2023-08-08 13:49:33 +0530

    Code Add/Update/Delete

--- Areas/LOC_Country/Controllers/LOC_CountryController.cs
+++ Areas/LOC_Country/Controllers/LOC_CountryController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPanel.Areas.LOC_Country.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace AdminPanel.Areas.LOC_Country.Controllers
 {
-    
+
     [Area("LOC_Country")]
     [Route("LOC_Country/[controller]/[action]")]
     public class LOC_CountryController : Controller
@@ -34,10 +35,46 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
             return View(table);
         }
 
-        public IActionResult LOC_CountryAddEdit()
+        public IActionResult LOC_CountrySave(LOC_CountryModel modelLOC_Country)
         {
-            
-            return View();
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            if (modelLOC_Country.CountryID == 0)
+            {
+                command.CommandText = "PR_Country_Insert";
+            }
+            else
+            {
+                command.CommandText = "PR_Country_UpdateByPK";
+                command.Parameters.Add("@CountryID", SqlDbType.Int).Value = modelLOC_Country.CountryID;
+            }
+            command.Parameters.Add("@CountryName", SqlDbType.VarChar).Value = modelLOC_Country.CountryName;
+            command.Parameters.Add("@CountryCode", SqlDbType.VarChar).Value = modelLOC_Country.CountryCode;
+            command.ExecuteNonQuery();
+            return View("LOC_Country");
+
+        }
+
+        public IActionResult LOC_CountryDelete(int CountryID)
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_Country_DeleteByPK";
+            command.Parameters.AddWithValue("@CountryID", CountryID);
+            command.ExecuteNonQuery();
+            return RedirectToAction("LOC_CountryList");
+        }
+
+        public IActionResult LOC_CountryAdd(int? CountryID)
+        {
+
+            return View("LOC_CountryAddEdit");
         }
     }
 }
