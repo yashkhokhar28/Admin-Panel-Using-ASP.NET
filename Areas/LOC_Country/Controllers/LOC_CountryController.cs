6c5ad651557f32af1d29c6bc218164e604f3fcd5
Yash Khokhar <yashkhokhar28@gmail.com>
2023-08-08 19:33:23 +0530

    Chnage

--- Areas/LOC_Country/Controllers/LOC_CountryController.cs
+++ Areas/LOC_Country/Controllers/LOC_CountryController.cs
@@ -10,10 +10,6 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
     [Route("LOC_Country/[controller]/[action]")]
     public class LOC_CountryController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
 
         private IConfiguration Configuration;
 
@@ -21,6 +17,8 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
         {
             Configuration = _configuration;
         }
+
+        #region SelectAll
         public IActionResult LOC_CountryList()
         {
             string connectionString = this.Configuration.GetConnectionString("ConnectionString");
@@ -34,7 +32,9 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
             table.Load(reader);
             return View(table);
         }
+        #endregion
 
+        #region Save
         public IActionResult LOC_CountrySave(LOC_CountryModel modelLOC_Country)
         {
             string connectionString = this.Configuration.GetConnectionString("ConnectionString");
@@ -42,22 +42,26 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
             connection.Open();
             SqlCommand command = connection.CreateCommand();
             command.CommandType = CommandType.StoredProcedure;
-            if (modelLOC_Country.CountryID == 0)
-            {
-                command.CommandText = "PR_Country_Insert";
-            }
-            else
-            {
-                command.CommandText = "PR_Country_UpdateByPK";
-                command.Parameters.Add("@CountryID", SqlDbType.Int).Value = modelLOC_Country.CountryID;
-            }
+            //if (modelLOC_Country.CountryID == 0)
+            //{
+                
+            //}
+            command.CommandText = "PR_Country_Insert";
+            //else
+            //{
+            //    command.CommandText = "PR_Country_UpdateByPK";
+            //    command.Parameters.Add("@CountryID", SqlDbType.Int).Value = modelLOC_Country.CountryID;
+            //}
             command.Parameters.Add("@CountryName", SqlDbType.VarChar).Value = modelLOC_Country.CountryName;
             command.Parameters.Add("@CountryCode", SqlDbType.VarChar).Value = modelLOC_Country.CountryCode;
+            command.Parameters.Add("@Modified", SqlDbType.DateTime).Value = DBNull.Value;
+            command.Parameters.Add("@Created", SqlDbType.DateTime).Value = DBNull.Value;
             command.ExecuteNonQuery();
-            return View("LOC_Country");
-
+            return View("LOC_CountryList");
         }
+        #endregion
 
+        #region Delete
         public IActionResult LOC_CountryDelete(int CountryID)
         {
             string connectionString = this.Configuration.GetConnectionString("ConnectionString");
@@ -70,11 +74,27 @@ namespace AdminPanel.Areas.LOC_Country.Controllers
             command.ExecuteNonQuery();
             return RedirectToAction("LOC_CountryList");
         }
+        #endregion
 
+        #region Add
         public IActionResult LOC_CountryAdd(int? CountryID)
         {
+            if(CountryID!=null)
+            {
+                string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "PR_Country_SelectByPK";
+                command.Parameters.Add("@CountryID", SqlDbType.Int).Value = CountryID;
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
 
+            }
             return View("LOC_CountryAddEdit");
         }
+        #endregion
     }
 }
--- Areas/LOC_Country/Models/LOC_CountryModel.cs
+++ Areas/LOC_Country/Models/LOC_CountryModel.cs
@@ -3,8 +3,8 @@
     public class LOC_CountryModel
     {
         public int CountryID { get; set; }
-        public string? CountryName { get; set; }
-        public string? CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
     }
