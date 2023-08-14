a82962e0d4515bc5b90bdfb29e1d8e86cb2c765d
Yash Khokhar <yashkhokhar28@gmail.com>
2023-08-14 17:49:03 +0530

    City CRUD

--- Areas/LOC_City/Controllers/LOC_CityController.cs
+++ Areas/LOC_City/Controllers/LOC_CityController.cs
@@ -1,12 +1,121 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminPanel.Areas.LOC_City.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace AdminPanel.Areas.LOC_City.Controllers
 {
+    [Area("LOC_City")]
+    [Route("LOC_City/[controller]/[action]")]
     public class LOC_CityController : Controller
     {
-        public IActionResult Index()
+        private IConfiguration Configuration;
+
+        public LOC_CityController(IConfiguration _configuration)
         {
-            return View();
+            Configuration = _configuration;
         }
+        #region SelectAll
+        public IActionResult LOC_CityList()
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_City_SelectAll";
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            connection.Close();
+            return View(table);
+        }
+        #endregion
+
+        #region SelectByID
+        public IActionResult LOC_CityListByID(int CityID)
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_City_SelectByPK";
+            command.Parameters.AddWithValue("CityID", CityID);
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            connection.Close();
+            return View(table);
+        }
+        #endregion
+
+        #region Save
+        public IActionResult LOC_CitySave(LOC_CityModel lOC_CityModel, int CityID = 0)
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            if (CityID == 0)
+            {
+                command.CommandText = "PR_City_Insert";
+            }
+            else
+            {
+                command.CommandText = "PR_City_UpdateByPK";
+                command.Parameters.AddWithValue("@CityID", CityID);
+            }
+            command.Parameters.AddWithValue("@CityName", lOC_CityModel.CityName);
+            command.Parameters.AddWithValue("@CityCode", lOC_CityModel.CityCode);
+            command.Parameters.AddWithValue("@StateID", lOC_CityModel.StateID);
+            command.Parameters.AddWithValue("@CountryID", lOC_CityModel.CountryID);
+            command.ExecuteNonQuery();
+            connection.Close();
+            return RedirectToAction("LOC_CityList");
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult LOC_CityDelete(int CityID)
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_City_DeleteByPK";
+            command.Parameters.AddWithValue("@CityID", CityID);
+            command.ExecuteNonQuery();
+            connection.Close();
+            return RedirectToAction("LOC_CityList");
+        }
+        #endregion
+
+        #region Add
+        public IActionResult LOC_CityAdd(int CityID = 0)
+        {
+            string connectionString = this.Configuration.GetConnectionString("ConnectionString");
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "PR_City_SelectByPK";
+            command.Parameters.AddWithValue("@CityID", CityID);
+            SqlDataReader reader = command.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            LOC_CityModel lOC_CityModel = new LOC_CityModel();
+            foreach (DataRow dataRow in table.Rows)
+            {
+                lOC_CityModel.CityName = dataRow["CityName"].ToString();
+                lOC_CityModel.CityCode = dataRow["CityCode"].ToString();
+                lOC_CityModel.StateID = Convert.ToInt32(dataRow["StateID"]);
+                lOC_CityModel.CountryID = Convert.ToInt32(dataRow["CountryID"]);
+            }
+            return View("LOC_CityAddEdit", lOC_CityModel);
+        }
+        #endregion
     }
 }
---
+++ Areas/LOC_City/Models/LOC_CityModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdminPanel.Areas.LOC_City.Models
+{
+    public class LOC_CityModel
+    {
+        public int? CityID { get; set; }
+
+        [Required]
+        public string? CityName { get; set; }
+        [Required]
+        public string? CityCode { get; set; }
+        public int? StateID { get; set; }
+        public int? CountryID { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
