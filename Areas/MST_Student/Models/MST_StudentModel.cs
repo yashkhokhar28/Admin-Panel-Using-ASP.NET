966ffaee44aaa49b9c1fcd8307f47a69e0ec0b26
Yash Khokhar <yashkhokhar28@gmail.com>
2023-08-29 20:45:37 +0530

    Update

--- Areas/MST_Student/Models/MST_StudentModel.cs
+++ Areas/MST_Student/Models/MST_StudentModel.cs
@@ -1,32 +1,36 @@
-﻿namespace AdminPanel.Areas.MST_Student.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdminPanel.Areas.MST_Student.Models
 {
     public class MST_StudentModel
     {
         public int StudentID { get; set; }
+        [Required]
+        [DisplayName("Student Name")]
+        public string StudentName { get; set; }
 
-        public string? StudentName { get; set; }
+        public string MobileNoStudent { get; set; }
 
-        public string? MobileNoStudent { get; set; }
+        public string Email { get; set; }
 
-        public string? Email { get; set; }
+        public string MobileNoFather { get; set; }
 
-        public string? MobileNoFather { get; set; }
+        public string Address { get; set; }
 
-        public string? Address { get; set; }
+        public DateTime BirthDate { get; set; }
 
-        public DateTime? BirthDate { get; set; }
+        public string Age { get; set; }
 
-        public string? Age { get; set; }
+        public string IsActive { get; set; }
 
-        public string? IsActive { get; set; }
+        public string Gender { get; set; }
 
-        public string? Gender { get; set; }
+        public string Password { get; set; }
 
-        public string? Password { get; set; }
+        public DateTime Created { get; set; }
 
-        public DateTime? Created { get; set; }
-
-        public DateTime? Modified { get; set; }
+        public DateTime Modified { get; set; }
 
         public int BranchID { get; set; }
 
@@ -36,15 +40,15 @@
 
     public class LOC_CityDropDownModel
     {
-        public int? CityID { get; set;}
+        public int CityID { get; set; }
 
-        public string? CityName { get; set; }
+        public string CityName { get; set; }
     }
 
     public class MST_BranchDropDownModel
     {
-        public int? BranchID { get; set; }
+        public int BranchID { get; set; }
 
-        public string? BranchName { get; set; }
+        public string BranchName { get; set; }
     }
 }
